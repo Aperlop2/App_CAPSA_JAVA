4e5f3e5bdb437bc4f481a33e79a24fc0ed6cdfd4
Tatianajimenez12 <tatiana_jr@tesch.edu.mx>
2025-01-07 11:51:09 -0600

    Configuracion y conexion de firebase con Android studio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -42,6 +44,8 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation ("com.google.android.gms:play-services-maps:19.0.0")
     implementation ("androidx.fragment:fragment:1.8.5")
-
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
 
 }
